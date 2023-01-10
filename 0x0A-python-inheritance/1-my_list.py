c5e6bde009700628df1c0d0062533e69e6649276
maranathaadesanya <adesanyamaranatha@yahoo.com>
2023-01-10 04:36:33 -0800

    Hot fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
+"""initialize"""
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
+    """create mi class mylist with inheritance"""
     def print_sorted(self):
-        """prints the sorted list"""
+        """sorted list"""
         print(sorted(self))
