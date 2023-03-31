83059d022edb8bb0148256d2e0c5da8ed763982e
maranathaadesanya <adesanyamaranatha@yahoo.com>
2023-03-31 13:12:15 +0000

    Hot fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Displays the size of the body of the response of a curl request
 
-curl -so /dev/null -w '%{size_download}\n' "$1"
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
