97af7127e0cbcfe1690f935b7c46123274b2cdef
maranathaadesanya <adesanyamaranatha@yahoo.com>
2023-04-08 13:03:09 +0000

    Debugging

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Displays the size of the body of the response of a curl request
 
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+#curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+
+#!/bin/bash
+
+url=$1
+response=$(curl -s -w "\n%{size_download}\n" "$url")
+size=$(echo "$response" | tail -n 1)
+
+echo "$size"
